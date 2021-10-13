b3fd880963ffa37eb7e6d521dcca93bf0462336d
LanaMinatchy <92440699+LanaMinatchy@users.noreply.github.com>
2021-10-13 17:27:05 +0200

    Update main.c

--- Projet_MMD.X/main.c
+++ Projet_MMD.X/main.c
@@ -13,10 +13,10 @@ int main (void) {
     InitTimer23();
     InitTimer1();
 
-// Configuration de séentres s o r t i e s
+// Configuration de sÃ©entres s o r t i e s
 
 InitIO();
-LED_BLANCHE = 0 ;
+LED_BLANCHE = 1 ;
 LED_BLEUE = 0 ;
 LED_ORANGE = 0 ;
 
